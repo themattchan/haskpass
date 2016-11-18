98039454701d94dfe003a360a77a2669f42a3ef3
Matthew Chan <matt@themattchan.com>
2016-11-18 02:14:40 -0800

    cleanup

--- src/AgileKeychain.hs
+++ src/AgileKeychain.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module AgileKeychain where
+
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Maybe
 
 import qualified Data.Map as M
 import qualified Data.Set as S
@@ -9,19 +12,27 @@ import qualified Data.Vector as V
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 
-import Control.Monad.Trans.Class
-import Control.Monad.Trans.Maybe
-
 import System.FilePath
 
-import OpenSSL.EVP.Base64
-import OpenSSL.EVP.Digest (pkcs5_pbkdf2_hmac_sha1)
-
-import qualified Network.URL as URL
 import qualified Data.Aeson as A
 import qualified Data.Aeson.Types as A
 import Data.Aeson ((.:), (.:?), (.!=))
+
+import qualified OpenSSL.EVP.Base64 as SSL (decodeBase64BS)
+import qualified OpenSSL.EVP.Digest as SSL (pkcs5_pbkdf2_hmac_sha1)
+
+import qualified Network.URL as URL
+
 --import Data.Attoparsec.ByteString
+
+--------------------------------------------------------------------------------
+-- * Types
+--------------------------------------------------------------------------------
+
+type ErrorMsg = String
+
+data KeyLevel = SL3 | SL5
+  deriving (Show,Eq, Bounded, Enum)
 
 newtype UUID
   = UUID { getUUID :: String }
@@ -36,7 +47,7 @@ type Section = String
 data ItemField = ItemField
   { name    :: String
   , value   :: Value
-  , valtype :: String
+  , valtype :: String      -- TODO make this typed
   } deriving (Show, Eq)
 
 data AgileKeychainItem = AgileKeychainItem
@@ -50,50 +61,30 @@ data AgileKeychainItem = AgileKeychainItem
   , notes     :: String
   } deriving (Show)
 
-data KeyLevel = SL3 | SL5
-  deriving (Show,Eq, Bounded, Enum)
-
-parseKeyLevel :: String -> Maybe KeyLevel
-parseKeyLevel "SL3" = Just SL3
-parseKeyLevel "SL5" = Just SL5
-parseKeyLevel _     = Nothing
-
-
 data AgileKeychainMasterKey = AgileKeychainMasterKey
-  { mk_Level :: KeyLevel
+  { mk_level :: KeyLevel
   , mk_id    :: UUID
   , mk_data  :: B.ByteString
   } deriving (Show, Eq)
 
 data AgileKeychain = AgileKeychain
-  { ak_level3Key  :: AgileKeychainMasterKey
-  , ak_level5Key  :: AgileKeychainMasterKey
+  { ak_vaultTitle :: String
   , ak_vaultPath  :: FilePath
-  , ak_vaultTitle :: String
+  , ak_level3Key  :: AgileKeychainMasterKey
+  , ak_level5Key  :: AgileKeychainMasterKey
   , ak_items      :: S.Set AgileKeychainItem
   } deriving (Show)
 
-
-type ErrorMsg = String
-
 {-@ type Salt = { v :: B.ByteString | bslen v == 8 } @-}
 type Salt = B.ByteString
-type RawKeyData = (Maybe Salt, B.ByteString)
 
-parseEncData :: B.ByteString -> Maybe RawKeyData
-parseEncData dat =
-  if B.length decoded < 8
-  then Nothing
-  else Just (salt, decodedData)
-  where
-    decoded = decodeBase64BS dat
-    hasSalt = "Salted__" `B.isPrefixOf` decoded
-    salt
-      | hasSalt = Just ((B.take 8 . B.drop 8) decoded)
-      | otherwise = Nothing
-    decodedData
-      | hasSalt = B.drop 16 decoded
-      | otherwise = decoded
+type DecodedData = (Maybe Salt, B.ByteString)
+
+newtype RawKeyData = RawKeyData (DecodedData, String, String, Int, KeyLevel)
+
+--------------------------------------------------------------------------------
+-- * Keychain parsing
+--------------------------------------------------------------------------------
 
 -- readKeychain :: FilePath -> IO (Either ErrorMsg AgileKeychain)
 readKeychain kcLoc masterPass =
@@ -109,12 +100,28 @@ readKeychain kcLoc masterPass =
       parsedKeys <- A.withArray "a single key" (mapM parseOneKey . V.toList) keyList
       return parsedKeys
 
-    parseOneKey :: A.Value -> A.Parser (Maybe (RawKeyData, String, String, Int, KeyLevel))
-    parseOneKey jsonVal = runMaybeT $ do
-      jsonObj       <- lift   $ A.withObject "get json object" return jsonVal
-      keyData       <- MaybeT $ parseEncData . B.pack <$> jsonObj .: "data"
-      keyId         <- lift   $ jsonObj .: "identifier"
-      keyValidation <- lift   $ jsonObj .: "validation"
-      keyIterations <- lift   $ max 1000 <$> jsonObj .:? "iterations" .!= 0
-      keyLevel      <- MaybeT $ parseKeyLevel <$> jsonObj .: "level"
-      return (keyData, keyId, keyValidation, keyIterations, keyLevel)
+
+parseKeyLevel :: String -> Maybe KeyLevel
+parseKeyLevel "SL3" = Just SL3
+parseKeyLevel "SL5" = Just SL5
+parseKeyLevel _     = Nothing
+
+parseOneKey :: A.Value -> A.Parser (Maybe RawKeyData)
+parseOneKey jsonVal = runMaybeT $ do
+  jsonObj       <- lift   $ A.withObject "get json object" return jsonVal
+  keyData       <- MaybeT $ decodeEncData . B.pack <$> jsonObj .: "data"
+  keyId         <- lift   $ jsonObj .: "identifier"
+  keyValidation <- lift   $ jsonObj .: "validation"
+  keyIterations <- lift   $ max 1000 <$> jsonObj .:? "iterations" .!= 0
+  keyLevel      <- MaybeT $ parseKeyLevel <$> jsonObj .: "level"
+  return $ RawKeyData(keyData, keyId, keyValidation, keyIterations, keyLevel)
+
+decodeEncData :: B.ByteString -> Maybe DecodedData
+decodeEncData dat
+  | B.length decoded < 8 = Nothing
+  | hasSalt              = Just (Just salt, B.drop 16 decoded)
+  | otherwise            = Just (Nothing, decoded)
+  where
+    decoded = SSL.decodeBase64BS dat
+    hasSalt = "Salted__" `B.isPrefixOf` decoded
+    salt    = (B.take 8 . B.drop 8) decoded
